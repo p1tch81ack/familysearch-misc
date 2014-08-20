65b8cc75310720653d75a78148ce4e71574e2259
Joe Shull <shullja@familysearch.org>
2014-08-20 10:25:24 -0600

    successfully getting connection with xmatters

--- splunktest/src/main/scala/org/familysearch/joetools/splunktest/App.scala
+++ splunktest/src/main/scala/org/familysearch/joetools/splunktest/App.scala
@@ -1,5 +1,7 @@
 package org.familysearch.joetools.splunktest
 
+import com.splunk.{Service, ServiceArgs}
+
 /**
  * @author ${user.name}
  */
@@ -8,8 +10,21 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    // Create a map of arguments and add login parameters
+    val loginArgs: ServiceArgs = new ServiceArgs()
+    loginArgs.setUsername("shullja")
+    loginArgs.setPassword("N3verGuess2014")
+    //https://splunk.vip.fsglobal.net
+//    loginArgs.setHost("localhost")
+    loginArgs.setHost("ut01-splunksch04.i.fsglobal.net")
+//    loginArgs.setScheme("HTTPS")
+    loginArgs.setPort(8089)
+//    loginArgs.setPort(443)
+//    loginArgs.setApp()
+
+    // Create a Service instance and log in with the argument map
+    val service: Service = Service.connect(loginArgs)
   }
+
 
 }
